4e4b54bc93554a773641d2a9b41be4677057cc2d
David Berscheid <david.berscheid@inwt-statistics.de>
2017-07-15 10:39:57 +0200

    final changes

--- QpatternSC/SCpatternD.R
+++ QpatternSC/SCpatternD.R
@@ -1,63 +1,64 @@
+#start with empty directory
+graphics.off()
+rm(list = ls(all = TRUE))
+
+# Load & install packages
+if (require("dplyr")) install.packages("dplyr")
+library(dplyr)
+if (require("stringr")) install.packages("stringr")
+library(stringr)
+
+# Load data set
+dataCompl = read.csv("data.csv")
+
+# Data preprocessing
+summary(dataCompl$CATEGORY)
+# delete the sum, which would lead to wrong results
+dataCompl = dataCompl[-812, ]
+dataCompl = dataCompl %>% group_by(CATEGORY) %>% mutate(sumTXs = sum(TXs))
+
+data = dataCompl[!duplicated(dataCompl$CATEGORY), ]
+data = data.frame(data$CATEGORY, data$sumTXs)
+data = data %>% rename(category = data.CATEGORY, txs = data.sumTXs)
+
+#set options
+options(digits = 2)
+
+dataPat     = dataCompl %>% group_by(PATTERNS) %>% mutate(sumTXspP = sum(TXs))
+patSummary  = dataPat[!duplicated(dataPat$PATTERNS), ]  #only picks one row per pattern 
+patSummary  = data.frame(patSummary$CATEGORY, patSummary$PATTERNS, patSummary$sumTXspP)
+patSummary  = patSummary %>% rename(category = patSummary.CATEGORY, pattern = patSummary.PATTERNS, txs = patSummary.sumTXspP)
+
+# define patterns
+patterns = c("DPT", "DPA", "Oracle", "Vote", "Kill", "Math")
+
+# define components
+finComponents   = data$category[c(2, 5, 7, 14, 20, 21, 18, 27, 23)]
+NotComponents   = data$category[c(9, 3)]
+WalComponents   = data$category[22]
+GameComponents  = data$category[c(19, 4, 19, 10, 13, 16, 17, 20)]
+LibComponents   = data$category[c(6, 26)]
 
 
-
-#part b)
-
-options(digits=2)
-
-
-dataPat = dataCompl %>% 
-  group_by(PATTERNS) %>% 
-  mutate(sumTXspP = sum(TXs))
-
-patSummary = dataPat[!duplicated(dataPat$PATTERNS),]    #only picks one row per pattern 
-
-patSummary = data.frame(patSummary$CATEGORY,patSummary$PATTERNS, patSummary$sumTXspP)
-patSummary = patSummary %>% 
-  rename(category = patSummary.CATEGORY, pattern = patSummary.PATTERNS, txs = patSummary.sumTXspP)
-
-
-
-
-#define patterns
-
-patterns = c("DPT","DPA","Oracle","Vote","Kill","Math")
-
-
-
-#define components
-finComponents = data$category[c(2,5,7,14,20,21,18, 27, 23)]
-NotComponents = data$category[c(9,3)]
-WalComponents = data$category[22]
-GameComponents = data$category[c(19,4,19,10,13,16,17,20)] 
-LibComponents = data$category[c(6,26)]
-
-
-for (j in 1:length(patterns))  {
-  
-  
-  #loop to get total amount of contracts per category
+#looping over all patterns and all categories
+for (j in 1:length(patterns)) {
+  # loop to get total amount of contracts per category
   Fin = c()
   TokFin = c()
-  
   for (i in 1:length(finComponents)) {
     Fin[i] = sum(dataPat$CATEGORY == as.character(finComponents[i]))
-    TokFin[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) &  str_count(dataPat$CATEGORY, as.character(finComponents[i])))
+    TokFin[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) & str_count(dataPat$CATEGORY, as.character(finComponents[i])))
     
   }
   Fin = sum(Fin)
   TokFin = sum(TokFin)
-  #percentage of contracts in that category using this pattern
   PercFinWTok = TokFin/Fin
-  
-  
   
   Not = c()
   TokNot = c()
-  
   for (i in 1:length(NotComponents)) {
     Not[i] = sum(dataPat$CATEGORY == as.character(NotComponents[i]))
-    TokNot[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) &  str_count(dataPat$CATEGORY, as.character(NotComponents[i])))
+    TokNot[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) & str_count(dataPat$CATEGORY, as.character(NotComponents[i])))
     
   }
   Not = sum(Not)
@@ -65,14 +66,11 @@ for (j in 1:length(patterns))  {
   
   PercNotWTok = TokNot/Not
   
-  
-  
-  
   Gam = c()
   TokGam = c()
   for (i in 1:length(GameComponents)) {
     Gam[i] = sum(dataPat$CATEGORY == as.character(GameComponents[i]))
-    TokGam[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) &  str_count(dataPat$CATEGORY, as.character(GameComponents[i])))
+    TokGam[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) & str_count(dataPat$CATEGORY, as.character(GameComponents[i])))
     
   }
   Gam = sum(Gam)
@@ -85,7 +83,7 @@ for (j in 1:length(patterns))  {
   TokWal = c()
   for (i in 1:length(WalComponents)) {
     Wal[i] = sum(dataPat$CATEGORY == as.character(WalComponents[i]))
-    TokWal[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) &  str_count(dataPat$CATEGORY, as.character(WalComponents[i])))
+    TokWal[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) & str_count(dataPat$CATEGORY, as.character(WalComponents[i])))
     
   }
   Wal = sum(Wal)
@@ -98,7 +96,7 @@ for (j in 1:length(patterns))  {
   TokLib = c()
   for (i in 1:length(LibComponents)) {
     Lib[i] = sum(dataPat$CATEGORY == as.character(LibComponents[i]))
-    TokLib[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) &  str_count(dataPat$CATEGORY, as.character(LibComponents[i])))
+    TokLib[i] = sum(str_count(dataPat$PATTERNS, as.character(patterns[j])) & str_count(dataPat$CATEGORY, as.character(LibComponents[i])))
     
   }
   Lib = sum(Lib)
@@ -106,30 +104,26 @@ for (j in 1:length(patterns))  {
   PercLibWTok = TokLib/Lib
   
   
-  #total amount of contracts
+  # total amount of contracts
   Total = sum(Fin, Not, Gam, Wal, Lib)
   
-  
-  
   if (j == 1) {
-    token = c(PercFinWTok,PercNotWTok,PercGamWTok, PercWalWTok, PercLibWTok )
+    token = c(PercFinWTok, PercNotWTok, PercGamWTok, PercWalWTok, PercLibWTok)
   } else if (j == 2) {
-    authorization = c(PercFinWTok,PercNotWTok,PercGamWTok, PercWalWTok, PercLibWTok )
+    authorization = c(PercFinWTok, PercNotWTok, PercGamWTok, PercWalWTok, PercLibWTok)
   } else if (j == 3) {
-    oracle = c(PercFinWTok,PercNotWTok,PercGamWTok, PercWalWTok, PercLibWTok )
+    oracle = c(PercFinWTok, PercNotWTok, PercGamWTok, PercWalWTok, PercLibWTok)
   } else if (j == 4) {
-    poll = c(PercFinWTok,PercNotWTok,PercGamWTok, PercWalWTok, PercLibWTok )
+    poll = c(PercFinWTok, PercNotWTok, PercGamWTok, PercWalWTok, PercLibWTok)
   } else if (j == 5) {
-    termination = c(PercFinWTok,PercNotWTok,PercGamWTok, PercWalWTok, PercLibWTok )
+    termination = c(PercFinWTok, PercNotWTok, PercGamWTok, PercWalWTok, PercLibWTok)
   } else {
-    math = c(PercFinWTok,PercNotWTok,PercGamWTok, PercWalWTok, PercLibWTok )
+    math = c(PercFinWTok, PercNotWTok, PercGamWTok, PercWalWTok, PercLibWTok)
   }
   
   
 }
 
-final = data.frame(category = c("Financial", "Notary", "Game", "Wallet","Library"), 
-                    token, authorization, oracle, poll, termination, math)
+final = data.frame(category = c("Financial", "Notary", "Game", "Wallet", "Library"), token, authorization, oracle, poll, termination, math)
 
-final
 
--- QusageSC/SCusagePI.R
+++ QusageSC/SCusagePI.R
@@ -1,57 +1,51 @@
-# Smart Contract Study 14.07.2017
+#start with empty directory
+graphics.off()
+rm(list = ls(all = TRUE))
 
 
-#Load & install packages 
-if(require("ggplot2")) install.packages("ggplot2"); library(ggplot2)
-if(require("dplyr")) install.packages("dplyr"); library(dplyr)
-if(require("stringr")) install.packages("stringr"); library(stringr)
+# Load & install packages
+if (require("ggplot2")) install.packages("ggplot2")
+library(ggplot2)
+if (require("dplyr")) install.packages("dplyr")
+library(dplyr)
+if (require("stringr")) install.packages("stringr")
+library(stringr)
 
-#Load data set
-dataCompl  <- read.csv("data.csv")
+# Load data set
+dataCompl = read.csv("data.csv")
 
 
-#Data preprocessing
+# Data preprocessing
 summary(dataCompl$CATEGORY)
-#delete the sum, which would lead to wrong results
-dataCompl <- dataCompl[-812,]
+# delete the sum, which would lead to wrong results
+dataCompl = dataCompl[-812, ]
+dataCompl = dataCompl %>% group_by(CATEGORY) %>% mutate(sumTXs = sum(TXs))
 
-dataCompl <- dataCompl %>% 
-  group_by(CATEGORY) %>% 
-  mutate(sumTXs = sum(TXs))
-
-#for part a) 
-data <- dataCompl[!duplicated(dataCompl$CATEGORY),]
-
-data <- data.frame(data$CATEGORY, data$sumTXs)
-data <- data %>% 
-  rename(category = data.CATEGORY, txs = data.sumTXs)
+# for part a)
+data = dataCompl[!duplicated(dataCompl$CATEGORY), ]
+data = data.frame(data$CATEGORY, data$sumTXs)
+data = data %>% rename(category = data.CATEGORY, txs = data.sumTXs)
 
 
-#combining subcategories to final categories
-Financial <- sum(data$txs[c(2,5,7,14,20,21,18, 27, 23)])
-Notary <- sum(data$txs[c(9,3)])
-Wallet <- data$txs[22]
-Game <- sum(data$txs[c(19,4,19,10,13,16,17,20)])
-Library <- sum(data$txs[c(6,26)])
-total <- sum(c(Financial, Notary, Wallet, Game, Library))
+# combining subcategories to final categories
+Financial = sum(data$txs[c(2, 5, 7, 14, 20, 21, 18, 27, 23)])
+Notary    = sum(data$txs[c(9, 3)])
+Wallet    = data$txs[22]
+Game      = sum(data$txs[c(19, 4, 19, 10, 13, 16, 17, 20)])
+Library   = sum(data$txs[c(6, 26)])
+total     = sum(c(Financial, Notary, Wallet, Game, Library))
 
-#setting options
-options(digits=2)
+# setting options
+options(digits = 2)
 
 
-distribution <- data.frame( 
-  names = c("Financial", "Notary", "Wallet", "Game", "Library"),
-  value = c(Financial, Notary, Wallet, Game, Library),
-  percentage = c(Financial/total, Notary/total, Wallet/total, Game/total, Library/total))
+distribution = data.frame(names = c("Financial", "Notary", "Wallet", "Game", "Library"), value = c(Financial, Notary, Wallet, Game, Library), 
+                          percentage = c(Financial/total, Notary/total, Wallet/total, Game/total, Library/total))
 
-#plot the distribution of Ethereum smart contract usage
-usagePlot <- ggplot(data=distribution, aes(distribution$names, distribution$percentage,  fill = "red")) +  
-  geom_bar(stat = "identity", show.legend = FALSE) +
-  ggtitle("Usage of Ethereum Smart Contracts by Categories") + 
-  xlab("Categories") +
-  ylab("Percentage") +
-  theme(axis.text=element_text(size=20),
-         axis.title=element_text(size=20,face="bold"))
-  
-#save plot in a file
-ggsave("usageContracts.png", dpi = 600, bg= "transparent")
+# plot the distribution of Ethereum smart contract usage
+usagePlot = ggplot(data = distribution, aes(distribution$names, distribution$percentage, fill = "red")) + geom_bar(stat = "identity", show.legend = FALSE) + 
+  ggtitle("Usage of Ethereum Smart Contracts by Categories") + xlab("Categories") + ylab("Percentage") + 
+  theme(axis.text = element_text(size = 20), axis.title = element_text(size = 20, face = "bold"))
+
+# save plot in a file
+ggsave("usageContracts.png", dpi = 600, bg = "transparent")
